b59e7fd35700fa400145acc8a328a97089386852
afra2604 <109967726+afra2604@users.noreply.github.com>
2023-08-30 14:57:12 +0700

    first commit

---
+++ resources/views/halamanUtama.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "stylesheet" href="css/style.css">
+    <title>Halaman_Utama</title>
+</head>
+<body>
+    <h1>Halaman Utama</h1>
+    <h1> <?= $name; ?></h1>
+    <h1> <?= $email; ?></h1>
+<script src="js/script.js"></script>
+</body>
+</html>
